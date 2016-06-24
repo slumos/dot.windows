5568f0b99d6b3f39cf02749e63956cc65fdf0db9
Steven Lumos <slumos@microsoft.com>
2016-06-24 11:29:39 -0700

    update gist

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,1 @@
-start http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/slumos/15c5f84211ec4b9edbb80bfad6640270/raw/0ddfdfd2970184e15ee09fe362e72fa032cf453c/slumos-boxstarter.ps1
+start http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/slumos/15c5f84211ec4b9edbb80bfad6640270/raw/376bf860df84e192738752109a02990413fdafed/slumos-boxstarter.ps1
