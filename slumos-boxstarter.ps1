a18bf11fde6a14cf3348537b711a08e4e18520ae
Steven Lumos <slumos@microsoft.com>
2016-06-24 14:15:50 -0700

    add lame idempotence

--- slumos-boxstarter.ps1
+++ slumos-boxstarter.ps1
@@ -66,17 +66,23 @@ Install-Package win32-openssh
 Install-Package Linqpad5
 
 # Visual Studio, the bear of unhelpful install grrr
-Start-Process -Wait `
-  -FilePath "\\products\public\PRODUCTS\Developers\Visual Studio 2015\Enterprise 2015.2\vs_enterprise.exe" `
-  -ArgumentList "/quiet /norestart /installselectableitems ProgrammingLanguages_Group;WindowsPlatformDevelopment_GroupV1;Node.js;GitForWindows;GitHubVS"
+if (!(test-path "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\")) {
+  Write-BoxstarterMessage "Start Visual Studio 2015 install."
+  Start-Process -Wait `
+    -FilePath "\\products\public\PRODUCTS\Developers\Visual Studio 2015\Enterprise 2015.2\vs_enterprise.exe" `
+    -ArgumentList "/quiet /norestart /installselectableitems ProgrammingLanguages_Group;WindowsPlatformDevelopment_GroupV1;Node.js;GitForWindows;GitHubVS"
 
-# The Azure .NET SDK will not see VS2015 until after a reboot. BECAUSE
-# WHY WOULD IT??
-if (Test-PendingReboot) { Invoke-Reboot }
+  # The Azure .NET SDK will not see VS2015 until after a reboot. BECAUSE
+  # WHY WOULD IT??
+  if (Test-PendingReboot) { Invoke-Reboot }
+}
+else {
+  Write-BoxstarterMessage "Visual Studio 2015 already installed."
+}
 
 # Then we install the SDK... which VS2015 will not see until after
 # another reboot. For real? Why are we so weak?
-Install-Package 'Microsoft Web Platform Installer'
+Install-Package webpi
 webpicmd.exe /Install /Products:Vs2015AzurePack.2.8 /SuppressReboot /AcceptEula /IISExpress /Verbose
 
 if (Test-PendingReboot) { Invoke-Reboot }
