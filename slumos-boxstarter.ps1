f71613dc83975b38b5c6f56256376ac72d3205b9
Steven Lumos <slumos@microsoft.com>
2016-07-14 12:42:08 -0700

    add sparkleshare package

--- slumos-boxstarter.ps1
+++ slumos-boxstarter.ps1
@@ -61,6 +61,7 @@ Install-Package npm
 Install-Package GoogleChrome
 Install-Package skyfonts
 Install-Package Git-Credential-Manager-for-Windows
+Install-Package sparkleshare
 
 Install-Package win32-openssh
 
