8886b6c206baf7f23165ac88efe78c8a525c1626
Steven Lumos <slumos@microsoft.com>
2017-03-14 09:22:28 -0700

    list of components for vs2017 install

---
+++ install-vs2017.ps1
@@ -0,0 +1,13 @@
+$installIds = @(
+  "Microsoft.VisualStudio.Workload.Azure",
+  "Microsoft.VisualStudio.Workload.Data",
+  "Microsoft.VisualStudio.Workload.NetCoreTools",
+  "Microsoft.VisualStudio.Workload.NetWeb",
+  "Microsoft.VisualStudio.Component.LinqToSql",
+  "Microsoft.VisualStudio.Component.TestTools.Core",
+  "Microsoft.VisualStudio.Component.TestTools.FeedbackClient",
+  "Microsoft.VisualStudio.Component.TestTools.MicrosoftTestManager",
+  "Microsoft.VisualStudio.Component.TestTools.WebLoadTest",
+  "Microsoft.VisualStudio.Component.TypeScript.2.0"
+)
+
